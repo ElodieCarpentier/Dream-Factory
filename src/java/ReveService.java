e52ac5fea733131b393674ff15fda55e0ea03170
Kevin_2 <Kevin_2@192.168.1.46>
2015-10-15 23:23:03 +0300

    Modif et ajout des boutons valider et non valider

--- src/java/ReveService.java
+++ src/java/ReveService.java
@@ -4,6 +4,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.application.FacesMessage;
@@ -250,7 +251,128 @@ public class ReveService {
             
         }
         return list;
-    } 
+    }
+    
+    
+    
+    public void envoiefinance(String nomdureve)throws SQLException, ClassNotFoundException{
+        RequestContext context = RequestContext.getCurrentInstance();
+        FacesMessage message2 = null;
+        Connection con = null;
+        Statement ps = null;
+        PreparedStatement ps2= null;
+        Statement ps3=null;
+        Class.forName( "com.mysql.jdbc.Driver" );
+        String url = "jdbc:mysql://localhost/dreamfactory";
+        String login= "root";
+        String passwd="";
+        try {
+            
+            con = DriverManager.getConnection(url, login, passwd);
+            ps = con.createStatement();
+            String sql= "UPDATE reve set RISQUE_REVE='"+risque+"',VALIDATION='en finance' where reve.IDREVE='"+nomdureve+"'";
+            ps.executeUpdate(sql);
+            
+            ps2= con.prepareStatement("Select * from reve where RISQUE_REVE=? and VALIDATION=?");
+            ps2.setString(1,risque);
+            ps2.setString(2, "en finance");
+            ResultSet rs2 = ps2.executeQuery();
+            
+             if (rs2.next()) // found
+            {
+                 message2 = new FacesMessage(FacesMessage.SEVERITY_INFO, "Le rêve a été envoyé pour étude budgétaire avec succès","");
+             }
+             else{
+                 message2 = new FacesMessage(FacesMessage.SEVERITY_WARN, "Erreur, le rêve n'a pas été envoyé au département des finances","");   
+             }
+            }
+            catch (Exception ex) {
+            System.out.println("Error in login() -->" + ex.getMessage());
+            
+          
+        }
+        FacesContext.getCurrentInstance().addMessage(null, message2);
+        
+    }
+    public void valider(String nomdureve)throws SQLException, ClassNotFoundException{
+        RequestContext context = RequestContext.getCurrentInstance();
+        FacesMessage message2 = null;
+        Connection con = null;
+        Statement ps = null;
+        PreparedStatement ps2= null;
+        Statement ps3=null;
+        Class.forName( "com.mysql.jdbc.Driver" );
+        String url = "jdbc:mysql://localhost/dreamfactory";
+        String login= "root";
+        String passwd="";
+        try {
+            
+            con = DriverManager.getConnection(url, login, passwd);
+            ps = con.createStatement();
+            String sql= "UPDATE reve set VALIDATION='proto' where reve.IDREVE='"+nomdureve+"'";
+            ps.executeUpdate(sql);
+            
+            ps2= con.prepareStatement("Select * from reve where IDREVE=? and VALIDATION=?");
+            ps2.setString(1,nomdureve);
+            ps2.setString(2, "proto");
+            ResultSet rs2 = ps2.executeQuery();
+            
+             if (rs2.next()) // found
+            {
+                 message2 = new FacesMessage(FacesMessage.SEVERITY_INFO, "Le rêve a été envoyé pour validation de la direction avec succès","");
+             }
+             else{
+                 message2 = new FacesMessage(FacesMessage.SEVERITY_WARN, "Erreur, le rêve n'a pas été envoyé à la direction","");   
+             }
+            }
+            catch (Exception ex) {
+            System.out.println("Error in login() -->" + ex.getMessage());
+            
+          
+        }
+        FacesContext.getCurrentInstance().addMessage(null, message2);
+        
+    }
+    
+    public void nonvalider(String nomdureve)throws SQLException, ClassNotFoundException{
+        RequestContext context = RequestContext.getCurrentInstance();
+        FacesMessage message2 = null;
+        Connection con = null;
+        Statement ps = null;
+        PreparedStatement ps2= null;
+        Statement ps3=null;
+        Class.forName( "com.mysql.jdbc.Driver" );
+        String url = "jdbc:mysql://localhost/dreamfactory";
+        String login= "root";
+        String passwd="";
+        try {
+            
+            con = DriverManager.getConnection(url, login, passwd);
+            ps = con.createStatement();
+            String sql= "UPDATE reve set VALIDATION='non' where reve.IDREVE='"+nomdureve+"'";
+            ps.executeUpdate(sql);
+            
+            ps2= con.prepareStatement("Select * from reve where IDREVE=? and VALIDATION=?");
+            ps2.setString(1,nomdureve);
+            ps2.setString(2, "non");
+            ResultSet rs2 = ps2.executeQuery();
+            
+             if (rs2.next()) // found
+            {
+                 message2 = new FacesMessage(FacesMessage.SEVERITY_INFO, "Le rêve n'a pas été valider","");
+             }
+             else{
+                 message2 = new FacesMessage(FacesMessage.SEVERITY_WARN, "Erreur, le rêve n'a pas pu être refusé","");   
+             }
+            }
+            catch (Exception ex) {
+            System.out.println("Error in login() -->" + ex.getMessage());
+            
+          
+        }
+        FacesContext.getCurrentInstance().addMessage(null, message2);
+        
+    }
     private void reinitialisation(){
         this.risque="";
         this.nom="";
